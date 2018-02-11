a57a2d6b2aa90d31ab0a1157ccd7ed73dc12ab7f
Jatupon Jantri <jameball007@gmail.com>
2018-02-11 11:35:29 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,17 @@
+function getFirstSelector(selector) {
+  var elements = document.querySelector(selector)
+  return elements
+}
+
+function nestedTarget() {
+  var target = document.querySelector('#nested div div div .target')
+  return target
+}
+
+function increaseRankBy(n) {
+  
+}
+
+function deepestChild() {
+  
+}
