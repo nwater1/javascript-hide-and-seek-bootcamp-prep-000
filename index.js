7985791417ea622871c080273166216b511d1947
Jatupon Jantri <jameball007@gmail.com>
2018-02-11 12:07:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ function nestedTarget() {
 function increaseRankBy(n) {
   
   var lists = document.querySelectorAll('.ranked-list')
-  var list1 = lists[0]
-  var list2 = lists[1]
+  var list1 = lists[0].children
+  var list2 = lists[1].children
   
   for(var i = 0; i < list1.length; i++) {
     list1[i].innerHTML = parseInt(i + 1 + n)
